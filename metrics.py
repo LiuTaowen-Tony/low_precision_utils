78aa531f1932eafaac168d9870f5f4cd2d727243
LiuTaowen Tony <tony_t_liu@outlook.com>
2024-05-26 16:44:23 +0100

    add model ema

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch import nn
 
 
@@ -14,18 +15,32 @@ def grad_on_batch(network, data, target, loss):
     network.eval()
     return {"grad_norm_batch": total_norm.item()}
 
+
+
+def grad_zero_percentage(network):
+    n_params = 0
+    for param in network.parameters():
+        n_params += param.numel()
+        n_zero = torch.sum(param == 0).item()
+    return {"zero_percentage": n_zero / n_params}
+
+
 def grad_on_trainset(network, dataset, batch_size, criterion):
     network.train()
     total_norm = 0
     network.zero_grad()
-    idx = torch.randperm(len(data))
-    idx = idx[:max(len(idx), 2048)]
-    for n, i in enumerate(range(0, len(idx), batch_size)):
-        data, target = dataset[idx[i:i + batch_size]]
+    dataset = torch.utils.data.Subset(dataset, range(0, 2048))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
+    zero_percents = []
+    for n, (data, target) in enumerate(dataloader):
+        data = data.cuda()
+        target = target.cuda()
         output = network(data)
         loss = criterion(output, target)
         loss.backward()
-    total_norm = nn.utils.clip_grad_norm_(network.parameters(), float('inf'))
+        total_norm += nn.utils.clip_grad_norm_(network.parameters(), float('inf'))
+        zero_percents.append(grad_zero_percentage(network)["zero_percentage"])
+        network.zero_grad()
     total_norm /= n
-    network.zero_grad()
-    return {"grad_norm_entire": total_norm.item()}
+    return {"grad_norm_entire": total_norm.item(), 
+            "zero_percentage": np.mean(zero_percents)}
--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
+import torch
+from torch import nn
 import qtorch
 from qtorch.quant import Quantizer
 import qtorch.quant
-import torch
+import copy
 import numpy
 
 
@@ -154,3 +156,27 @@ class MasterWeightOptimizerWrapper():
         self.scheduler.step()
         return {"loss": numpy.mean(losses),  "acc": numpy.mean(acces), "grad_norm": grad_norm.item()}
 
+
+class ModelEma(nn.Module):
+    def __init__(self, model, decay=0.9999, device=None):
+        super(ModelEma, self).__init__()
+        # make a copy of the model for accumulating moving average of weights
+        self.module = copy.deepcopy(model)
+        self.module.eval()
+        self.decay = decay
+        self.device = device  # perform ema on different device from model if set
+        if self.device is not None:
+            self.module.to(device=device)
+
+    def _update(self, model, update_fn):
+        with torch.no_grad():
+            for ema_v, model_v in zip(self.module.state_dict().values(), model.state_dict().values()):
+                if self.device is not None:
+                    model_v = model_v.to(device=self.device)
+                ema_v.copy_(update_fn(ema_v, model_v))
+
+    def update(self, model):
+        self._update(model, update_fn=lambda e, m: self.decay * e + (1. - self.decay) * m)
+
+    def set(self, model):
+        self._update(model, update_fn=lambda e, m: m)
