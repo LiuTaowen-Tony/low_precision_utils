8e9066dca477e9ad723975a0b88956cbe984fe69
Tony <tony_t_liu@outlook.com>
2025-04-08 21:20:29 +0100

    update

--- quant.py
+++ quant.py
@@ -101,7 +101,7 @@ class FPQuant(QuantMethod):
 
 @dataclass(frozen=True)
 class IntQuant(QuantMethod):
-    wl : int = 23
+    wl : int = 24
     fl : int = 8
     clamp : bool = True
     symmetric : bool = False
